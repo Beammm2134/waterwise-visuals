dbd383f97fdc202ab26126a685fffaaa265ce231
Kongpop Boonma <kongpopboonma1912@gmail.com>
2024-11-30 17:56:01 +0700

    change port

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,11 +8,16 @@ dotenv.config();
 dotenv.config({ path: './config/config.env' });
 const app = express();
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+{
+    origin: process.env.HOST || 'https://lovable.dev/projects/b7674f45-d296-4037-b195-c51d01b405cb',
+    credentials: true
+}));
+    
 
 // Routes
 app.use("/api/plants", plantRoutes);
